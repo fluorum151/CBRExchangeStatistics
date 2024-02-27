856a430c090cee066a6f9969e5ef056ed5e465b9
fluorum151 <62394171+fluorum151@users.noreply.github.com>
2024-02-27 10:42:21 +0300

    Move currencies to .env

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import os
 from dotenv import load_dotenv
 from currency import Currency
 import datetime
+import os
+import json
 
 
 load_dotenv()
@@ -12,20 +13,12 @@ date1 = date2 - datetime.timedelta(days=90)
 date1 = date1.strftime("%d/%m/%Y")
 date2 = date2.strftime("%d/%m/%Y")
 
-usd, eu, chf, gbp, jpy, cny, aud, cnd = (Currency('R01235', 'USD'),
-                                         Currency('R01239', 'EU'),
-                                         Currency('R01775', 'CHF'),
-                                         Currency('R01035', 'GBP'),
-                                         Currency('R01820', 'JPY'),
-                                         Currency('R01375', 'CNY'),
-                                         Currency('R01010', 'AUD'),
-                                         Currency('R01350', 'CND'))
-
-CURRENCIES = (usd, eu, chf, gbp, jpy, cny, aud, cnd)
+CURRENCIES = json.loads(os.environ['CURRENCIES'])
 
 
 if __name__ == '__main__':
-    for currency in CURRENCIES:
+    for curr in CURRENCIES:
+        currency = Currency(curr[0], curr[1])
         currency.get_currency(date1, date2)
         currency.get_exchange_data()
         print(currency)
